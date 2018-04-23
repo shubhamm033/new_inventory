093294540c36e52465e279dd37ecd7da6b8781f7
Shubham <shubham.mahawar@qcin.org>
2018-04-24 01:18:49 +0530

    fixed

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 
 from flask_pymongo import MongoClient
-# mongo_ip = "172.17.0.8"
+mongo_ip = "172.17.0.8"
 mongo_ip = "localhost"
 mongo_port = 27017
 uri  = "mongodb://" + mongo_ip + ":" + str(mongo_port)+"/"
 mongo = MongoClient(uri)
 
-inventory=mongo['inventory']
+inventory=mongo['inventory1']
 
 
 
